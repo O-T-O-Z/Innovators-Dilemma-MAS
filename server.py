766d965f25a5c652222240e43ece2e4d253d42b5
O-T-O-Z <omertarikozyilmaz@gmail.com>
2022-09-26 12:02:42 +0200

    spawning agents working

--- server.py
+++ server.py
@@ -1,17 +1,37 @@
+from src.agents.company import CompanyAgent
 import mesa
 from src.models.market import MarketModel
+from src.agents.company import CompanyAgent
+#from test import MarketModel
+
+WIDTH = 10
+HEIGHT = 10
 
 def agent_portrayal(agent):
-    portrayal = {"Shape": "circle",
-                 "Filled": "true",
-                 "Layer": 0,
-                 "r": 0.5}
+    color = None
+    if isinstance(agent, CompanyAgent):
+        color = "red"
+    else:
+        color = "green"
+    return {
+        "Shape": "circle",
+        "Filled": "true",
+        "Layer": 0,
+        "r": 0.5,
+        "Color": color
+    }
 
-    return portrayal
-
-grid = mesa.visualization.CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+grid = mesa.visualization.CanvasGrid(agent_portrayal, WIDTH, HEIGHT, 500, 500)
 server = mesa.visualization.ModularServer(
-    MarketModel, [grid], "Market Model", {"num_companies": 5, "num_customers": 20, "width": 10, "height": 10}
+    MarketModel, 
+    [grid], 
+    "Market Model", {
+        "num_companies": 5, 
+        "num_customers": 20, 
+        "width": WIDTH, 
+        "height": HEIGHT
+    }
 )
+
 server.port = 8521 # The default
 server.launch()
--- src/models/market.py
+++ src/models/market.py
@@ -1,4 +1,4 @@
-from typing import Type
+from typing import Type, List
 import mesa
 import random
 import numpy as np
@@ -22,18 +22,20 @@ class MarketModel(Model):
         self.height = height
 
         self.reset()
+
+    def reset(self):
         self.datacollector = mesa.DataCollector(
             model_reporters={}, 
             agent_reporters={}
         )
-
-    def reset(self):
         self.grid = mesa.space.MultiGrid(self.width, self.height, True)
         self.schedule = mesa.time.RandomActivation(self)
         self.running = True # maybe this variable is important?
         self.companies = {}
         self.customers = {}
         self.free_cells = [x for x in range(self.width * self.height)]
+        self.next_id = 0
+        self._spawn_agents()
     
     # Getters
     def get_companies(self):
@@ -48,23 +50,29 @@ class MarketModel(Model):
     def get_customer(self, id):
         return self.customers[id]
 
+    def get_next_id(self):
+        val = self.next_id
+        self.next_id += 1
+        return val
+
     # Environment logic
 
     def _spawn_agents(self):
-        self._spawn_agents_of_class(self.num_companies, CompanyAgent)
-        self._spawn_agents_of_class(self.num_customers, CustomerAgent)
+        self._spawn_agents_of_class(self.num_companies, CompanyAgent, self.companies)
+        self._spawn_agents_of_class(self.num_customers, CustomerAgent, self.customers)
 
     def _get_free_cell_pos(self):
-        rand_idx = random.randint(0, len(self.free_cells))
-        self.free_cells.pop(rand_idx)
-        return np.array(rand_idx % self.width, rand_idx / self.width)
+        rand_idx = random.randint(0, len(self.free_cells) - 1)
+        val = self.free_cells.pop(rand_idx)
+        return np.array([val % self.width, val // self.width])
 
-    def _spawn_agents_of_class(self, n, class_: Type):
-        for id in range(n):
+    def _spawn_agents_of_class(self, n, class_: Type, ls: List):
+        for _ in range(n):
+            id = self.get_next_id()
             agent = class_(id, self, self._get_free_cell_pos())
+            ls[id] = agent
             self.schedule.add(agent)
-            self.grid.place_agent(agent, agent.position)
-            self.companies.append(agent)
+            self.grid.place_agent(agent, tuple(agent.position))
 
     def step(self):
         self.datacollector.collect(self)
